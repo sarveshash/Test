9843f5575225dad350aeacc3f1451a240080512f
sarveshash <136432405+sarveshash@users.noreply.github.com>
2025-07-14 08:14:01 +0530

    Update x.py

--- x.py
+++ x.py
@@ -1,8 +1,12 @@
+from moviepy.editor import ImageClip
 from PIL import Image
 import numpy as np
 
+# Load the background image
 img = Image.open("xyz.png").convert("RGB")
 img = img.resize((500, 500))  # arbitrary size
 img_np = np.array(img)
 
-print(img_np.shape)
+# Create ImageClip and export as video
+clip = ImageClip(img_np).set_duration(3)
+clip.write_videofile("bg_only.mp4", codec="libx264", fps=24)
