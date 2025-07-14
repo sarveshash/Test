d838f159d7b24d2e41a7cae1e18ef217c4fbd057
sarveshash <136432405+sarveshash@users.noreply.github.com>
2025-07-14 08:17:50 +0530

    Update x.py

--- x.py
+++ x.py
@@ -2,11 +2,9 @@ from telethon.sync import TelegramClient
 
 api_id = 27715449
 api_hash = "dd3da7c5045f7679ff1f0ed0c82404e0"
-bot_token = "7981770051:AAH5isv89k-20WiAXJZwW7hjaG0S6Dvrkdg"
 
-# Start client
-bot = TelegramClient("send_to_me", api_id, api_hash).start(bot_token=bot_token)
+client = TelegramClient("user_session", api_id, api_hash)
+client.start()  # You'll be prompted for phone number + Telegram code
 
-# Send to your Saved Messages
-bot.send_file("me", "bg_only.mp4", caption="🎬 Here's your generated video.")
+client.send_file("me", "bg_only.mp4", caption="🎬 Here's your video from VPS.")
 print("✅ Sent to your Telegram Saved Messages.")
